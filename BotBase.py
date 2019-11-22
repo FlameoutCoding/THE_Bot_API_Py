130556e499b04fe7f3ac7cd1c4990fd459d98dec
Monika 'Flameout' Hackenberg <flameout@ai-arena.de>
2019-11-22 19:29:32 +0100

    FIXED - some bugs (already :))

--- BotBase.py
+++ BotBase.py
@@ -3,11 +3,11 @@ import socket
 class BotBase:
     def __init__(self,host,port,token):
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.socket.connect(host,port)
+        self.socket.connect((host,port))
         self.authToken = token
 
     def authenticate(self,goIntoReadLoop=True):
-        self.socket.send("{\"identifier\":\""+self.authToken+"\"}")
+        self.socket.send("{\"identifier\":\""+self.authToken+"\"}\n")
         if goIntoReadLoop:
             self.readLoop()
 
@@ -17,9 +17,9 @@ class BotBase:
             data = self.socket.recv(1024)
             buffer += str(data)
             while "\n" in buffer:
-                message = buffer[:"\n" in buffer]
+                message = buffer[:buffer.index("\n")]
                 self.processMessage(message)
-                message = message["\n" in buffer+1:]
+                message = message[buffer.index("\n")+1:]
 
     def processMessage(self,message):
         print "I got a message: "+message
